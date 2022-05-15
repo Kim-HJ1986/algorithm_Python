b85ef8e0374de5f81bf29c519c79c324b4e8c673
Jun Kim <rlafbf222@naver.com>
2022-05-15 23:14:43 +0900

    1주차 강의 과제 완료_7일차

--- week_1/08_find_count_to_turn_out_to_all_zero_or_all_one.py
+++ week_1/08_find_count_to_turn_out_to_all_zero_or_all_one.py
@@ -1,10 +1,17 @@
-input = "011110"
+input = "000110011"
 
 
 def find_count_to_turn_out_to_all_zero_or_all_one(string):
     sList = list(string)
-    print(sList.count("0"))
-    return 1
+    # 0과 1중 더 적은 값 구하기
+    min_num = "1" if sList.count("0") >= sList.count("1") else "0"
+
+    cnt=0
+    for s in string:
+        if s == min_num: # 만약 적은 값이라면 cnt += 1
+            cnt+=1
+
+    return cnt
 
 
 result = find_count_to_turn_out_to_all_zero_or_all_one(input)
